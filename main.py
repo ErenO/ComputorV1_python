39fa09d29ef56cd926403c04c695da30a9114e78
Eren OZDEK <eozdek@e2r3p1.42.fr>
2017-07-31 12:00:20 +0200

    rajout de la couleur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from colors import *
 import re
 import sys
 import math
@@ -17,33 +18,48 @@ def delta_positive(a, b, delta, c):
     else:
         x1 = 0
         x2 = 0
+    sys.stdout.write(BLUE)
     print "Le discriminant est strictement positif. Les deux solutions sont :"
+    sys.stdout.write(RED)
     print "x1 =", x1 #.as_integer_ratio())))
     print "x2 =", x2
+    sys.stdout.write(RESET)
 
 def delta_negative(b, delta, a):
+    sys.stdout.write(RED)
     print "Le discriminant est negatif"
+    sys.stdout.write(BLUE)
     print "Δ < 0 alors l'équation ne possède pas de solution réelle mais admet 2 solutions complexes x1 et x2"
+    sys.stdout.write(RED)
     print "x1 = (−", b, "− i√(", -delta,") ) / (", 2 * a,") et x2 = (−", b,"+ i√(", -delta,") ) / (", 2 * a,")"
+    sys.stdout.write(RESET)
 
 def delta_zero(a, b):
+    sys.stdout.write(BLUE)
     print "Le discriminant est égal à zero. La solution est :"
+    sys.stdout.write(RED)
     x0 = 0
     if a != 0:
         x0 = (-b) / (2 * a)
     else:
         print "On peut pas diviser par zero"
     print "x =", x0
+    sys.stdout.write(RESET)
 
 def calc_first_degrees(zero, one):
+    sys.stdout.write(RED)
     print "Polynomial degree: 1"
     result = (-zero) / one
+    sys.stdout.write(BLUE)
     print "Il y a une solution:"
     print "x =", result
+    sys.stdout.write(RESET)
 
 def calc_second_degrees(a, b, c):
     delta = abs(b * b) - (4 * a * c)
+    sys.stdout.write(RED)
     print "Polynomial degree: 2"
+    sys.stdout.write(GREEN)
     print "delta =", delta
     if delta > 0:
         delta_positive(a, b, delta, c)
@@ -51,31 +67,30 @@ def calc_second_degrees(a, b, c):
         delta_zero(a, b)
     elif delta < 0:
         delta_negative(b, delta, a)
+    sys.stdout.write(RESET)
 
 def reduced_form(tab, exp):
     index = 0
-    nb = 0
-    print tab
-    print exp
+    sys.stdout.write(BLUE)
     s = "Forme Réduite: "
-    if (min(tab) != 0 and max(tab) != 0):
-        for i in exp:
-            if tab[i] != 0:
-                nb = tab[i]
-                if index > 0:
-                    if tab[i] > 0 :
-                        s += " + "
-                    else:
-                        s += " - "
-                    nb = abs(tab[i])
-                s += str(nb)
-                s += "* X ^ "
-                s += str(i)
-                index += 1
-    else:
-        s += "0"
+    for i in tab:
+        if i != 0:
+            if index > 0 and index < len(tab):
+                if i > 0:
+                    s += " + "
+                else:
+                    s += " - "
+                i = abs(i)
+            s += str(i)
+            s += " * X ^ "
+            s += str(index)
+        else:
+            if max(tab) == 0:
+                s += "0"
+        index += 1
     s += (" = 0\n")
-    print(s)
+    print (s)
+    sys.stdout.write(RESET)
 
 def calc_by_degrees(exp, nb):
     tab = [0] * (abs(min(exp)) + max(exp) + 1)
@@ -84,11 +99,11 @@ def calc_by_degrees(exp, nb):
     for exo in exp:
         tab[exo] += nb[i]
         i += 1
-    print nb
     if (min(nb) != 0 and max(nb) != 0):
         reduced_form(tab, exp)
     else:
         print "Reduced form: 0 = 0"
+    sys.stdout.write(GREEN)
     if isInt == 1:
         print "Le programme ne gère pas les exposants non entiers"
     elif max(exp) == 0 and tab[0] == 0:
@@ -103,23 +118,35 @@ def calc_by_degrees(exp, nb):
         calc_first_degrees(tab[0], tab[1])
     else:
         print "L'equation est supérieur à un degrée 2, il est de degree", max(exp)
+    sys.stdout.write(RESET)
 
 def handle_param(befExp, befNb, aftExp, aftNb):
     i = 0
     exp = []
     nb = []
     pb = 0
+    expNb = 0
     if len(befExp) > 0 and len(befNb) > 0 or len(aftExp) > 0 and len(aftNb) > 0:
         for bexp in befExp:
             if bexp.replace(" ", "").find(".") == -1:
-                exp.append(int(bexp.replace(" ", "")))
+                expNb = int(bexp.replace(" ", ""))
+                if expNb > 10000:
+                    print "Problème d'exposants"
+                    pb = 1
+                else:
+                    exp.append(expNb)
             else:
                 if pb == 0:
                     print "Problème d'exposants"
                     pb = 1
         for aexp in aftExp:
             if aexp.replace(" ", "").find(".") == -1:
-                exp.append(int(aexp.replace(" ", "")))
+                expNb = int(aexp.replace(" ", ""))
+                if expNb > 10000:
+                    print "Problème d'exposants"
+                    pb = 1
+                else:
+                    exp.append(expNb)
             else:
                 if pb == 0:
                     print "Problème d'exposants"
@@ -132,7 +159,7 @@ def handle_param(befExp, befNb, aftExp, aftNb):
             if min(exp) >= 0:
                 calc_by_degrees(exp, nb)
             else:
-                print "Progromme ne gere pas les exposants negatifs"
+                print "Programme ne gere pas les exposants negatifs"
 def main(argv):
     if len(argv) > 1:
         befEqu = re.search('(.)+(?=\=)', argv[1])
@@ -144,8 +171,6 @@ def main(argv):
         if len(befExp) == len(befNb) and len(aftExp) == len(aftNb):
             handle_param(befExp, befNb, aftExp, aftNb)
         else:
-            print (len(befExp) == len(befNb))
-            print (len(aftExp) == len(aftNb))
             print "Problème d'exposants"
     else:
         line = sys.stdin.readline()
