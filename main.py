b0b4e4b8ae632bb02acbf7f217e18aee4b484e9a
Eren OZDEK <eozdek@e3r6p20.42.fr>
2017-08-26 19:06:35 +0200

    regex fonctionnelle et ajout fct sqrt

--- main.py
+++ main.py
@@ -9,12 +9,36 @@ import collections
 # pour les exposants (?<=\^)((\d)+)
 # pour le coeff (\d)+(?=\s\*)
 
+# double square_root(double num)
+# {
+#   double x1;
+#   double x2;
+#
+#   x1 = (num * 1.0) / 2;
+#   x2 = (x1 + (num / x1)) / 2;
+#   while (ABS(x1 - x2) >= 0.0000001)
+#   {
+#     x1 = x2;
+#     x2 = (x1 + (num / x1)) / 2;
+#   }
+#   return (x2);
+# }
+
+
+def sqrt(num):
+    x1 = (num * 1.0) / 2
+    x2 = (x1 + (num / x1)) / 2
+    while (abs(x1 - x2) > 0):
+        x1 = x2
+        x2 = (x1 + (num / x1)) / 2
+    return x2
+
 def delta_positive(a, b, delta, c):
     x1 = 0
     x2 = 0
     if a != 0:
-        x1 = (-b - math.sqrt(delta)) / (2 * a)
-        x2 = (-b + math.sqrt(delta)) / (2 * a)
+        x1 = (-b - sqrt(delta)) / (2 * a)
+        x2 = (-b + sqrt(delta)) / (2 * a)
     else:
         x1 = 0
         x2 = 0
